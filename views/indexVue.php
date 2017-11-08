be7522d3d58e1da75d29c2e1b185cfa11415dbde
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-11-08 14:03:16 +0100

    add table in index view

--- views/indexVue.php
+++ views/indexVue.php
@@ -2,7 +2,35 @@
   include("template/header.php")
  ?>
 
-<p>This is the index view</p>
+ <div class="container TabList">
+     <h2 class="text-center">Account</h2>
+   <table class="table table-striped table-responsive text-center">
+     <thead>
+       <tr>
+         <th>User number</th>
+         <th>Account name</th>
+         <th>User name</th>
+         <th>Amount</th>
+         <th>Bank tansactions</th>
+       </tr>
+     </thead>
+     <tbody>
+         <!-- <?php foreach ($accounts as $account){
+           echo "<tr>";
+           echo "<td>".$account->get()."</td>";
+           echo "<td>".$account->get()."</td>";
+           echo "<td>".$account>get()."</td>";
+           echo "<td><a class='btn btn-custom btn-sm' href='../controllers/?id=".$account->getId()."'>Transfer</a>
+           <a class='btn btn-custom btn-sm' href='../controllers/?id=".$account->getId()."'>Withdraw</a>
+           <a class='btn btn-custom btn-sm' href='../controllers/?id=".$account->getId()."'>Payment</a>
+           <a class='btn btn-custom btn-sm' href='../controllers/?id=".$account->getId()."'>Delete</a></td>";
+           echo "</tr>";
+         } ?> -->
+     </tbody>
+   </table>
+   </div>
+ </main>
+
 
  <?php
    include("template/footer.php")
