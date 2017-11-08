e81292707e9728832192dd2d8dfe12bbb6ace68a
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-11-08 11:43:54 +0100

    add header and footer css

--- views/template/header.php
+++ views/template/header.php
@@ -2,9 +2,14 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Your site title</title>
+    <title>Bank account</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
   </head>
   <body>
     <header>
-      <h1>Main title in the header</h1>
+      <!-- <div class="container-fluid">         -->
+      <h1 class="text-center jumbotron">Bank account</h1>
+    <!-- </div> -->
     </header>
