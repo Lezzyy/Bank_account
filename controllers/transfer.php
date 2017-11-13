d703d5105917448923f165842b1d9e6dd0203e2f
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-11-13 16:34:47 +0100

    transfer button ok

--- controllers/transfer.php
+++ controllers/transfer.php
@@ -12,9 +12,56 @@ if(isset($_GET['number'])){
   include('../views/transferView.php');
 }
 
-// if(isset($_POST['account'] && isset($_POST['transferAccount']))){
+
+if (isset($_POST['Amount']) && $_POST['Amount']>0){
+ // if(isset($_POST['account'])){
+    // we get and show the account selected
+    $account = $manager->getAccount($_POST['account']);
+    // we get the initial amount
+    $accountFrom = $account->getAmount();
+
+    $secondAccount = $manager->getAccount($_POST['transferAccount']);
+    $accountTo = $secondAccount->getAmount();
+
+ if($_POST['Amount']<= $accountFrom){
+       $secondAccount->paymentAmount($_POST['Amount']);
+       $account -> withdrawAmount($_POST['Amount']);
+
+       $manager-> updateAmount($secondAccount);
+       $manager -> updateAmount($account);
+         header('Location: index.php');
+ } else {
+  echo 'not ok';
+}
+}
+
 //
+// if(isset($_POST['account'])){
+//   // we get and show the account selected
+//   $account = $manager->getAccount($_POST['id']);
+//   // we get the initial amount
+//   $accountFrom = $account->getAmount();
+//   var_dump($account);
+//   include('../views/transferView.php');
+//  if (isset($_POST['transferAccount'])){
+//   $secondAccount = $manager->getAccount($_POST['id']);
+//   $accountTo = $secondAccount->getAmount();
+//  if (isset($_POST['Amount'])){
+//   if ($_POST['Amount']<=$accountFrom){
+//     $secondAccount->paymentAmount($_POST['amount']);
+//     $account -> withdrawAmount($_POST['amount']);
+//
+//     $manager-> updateAmount($secondAccount);
+//     $manager -> updateAmount($account);
+//
+//     // include('../views/indexView.php');
+//   }else {
+//     echo "enter a correct value";
+//   }
+//   }
+//   }
 // }
+
 // if(isset($_GET['number'])){
 //   $accounts = $manager->getUserAccounts($_GET['number']);
 //   var_dump($accounts);
--- views/withdrawView.php
+++ views/withdrawView.php
@@ -4,7 +4,6 @@ include("template/header.php");
  <main>
    <div class="container withdraw">
      <div class="card" style="width: 20rem;">
-  <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
   <div class="card-block">
     <h4 class="card-title text-center">Withdraw</h4>
     <p class="card-text"><strong>User number :</strong> <?php echo $account->getNumber();?></p>
@@ -14,7 +13,6 @@ include("template/header.php");
 
     <form class="" action="../controllers/withdraw.php" method="post">
     <input type="text" name="amount" value="" placeholder="Amount to withdraw in €">
-    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
     <input type="hidden" name="id" value="<?php echo $account->getId();?>">
     <input type="submit" name="register" value="Withdraw" class="btn btn-custom">
   </form>
